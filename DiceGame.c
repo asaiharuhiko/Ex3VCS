cb682eb8df5d546cd2f24d4c08926d7b4fc907c0
ASAI Haruhiko <s1320047@u-aizu.ac.jp>
2025-06-28 11:34:19 +0900

    Won/lost message modified

--- DiceGame.c
+++ DiceGame.c
@@ -13,7 +13,7 @@ int main(){
     scanf("%s", name);
     printf("Hello, %s!\n", name);
 
-    printf("Rolling dice...\n");
+    printf("Rolling the dice...\n");
     for(int i = 0 ; i < 2 ; i++){
         d[i] = rand() % 7;
         total += d[i];
@@ -21,9 +21,9 @@ int main(){
     }
     printf("Total value: %d\n", total);
     if(total > 7){
-        printf("You won\n");
+        printf("%s won!\n",name);
     } else{
-        printf("You lost\n");
+        printf("%s lost!\n",name);
     }
 
     return 0;
